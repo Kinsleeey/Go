259070fd9583640b78ab042ee0e41fbb58d004a7
KE-labs <kchukwuma28@gmail.com>
2026-05-10 12:22:39 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+import Header from './Header';
+import Homepage from './Homepage';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Homepage />
+    </>
+  )
+}
+
+export default App
