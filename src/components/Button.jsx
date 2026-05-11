abba6b31498f367f9deb8ae71fc6db64c4cf0ecb
KE-labs <kchukwuma28@gmail.com>
2026-05-11 16:20:18 +0100

    tray double btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,8 +11,18 @@ export default function Button(props) {
 
 export function RoundButton (props) {
     return (
-        <button className={`add-btn ${props.extraClass}`} onClick={props.click}>
+        <button className={`add-btn red-add-btn ${props.extraClass}`} onClick={props.click}>
             {props.svg}
         </button>
+    )
+}
+
+export function CrementButton () {
+    return (
+        <div class="qty" role="group" aria-label="Quantity">
+            <button class="add-btn white-add-btn" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#bb0020" viewBox="0 0 256 256"><path d="M228,128a12,12,0,0,1-12,12H40a12,12,0,0,1,0-24H216A12,12,0,0,1,228,128Z"></path></svg></button>
+            <span class="qty__num">1</span>
+            <RoundButton svg={<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" viewBox="0 0 256 256"><path d="M228,128a12,12,0,0,1-12,12H140v76a12,12,0,0,1-24,0V140H40a12,12,0,0,1,0-24h76V40a12,12,0,0,1,24,0v76h76A12,12,0,0,1,228,128Z"></path></svg>}/>
+        </div>
     )
 }
